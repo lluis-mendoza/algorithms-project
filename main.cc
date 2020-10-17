be57db56edd381f92a44c8da5b075686c53072e3
lluis-mendoza <lluis.mendoza.vandrell1030@gmail.com>
2020-10-17 22:43:12 +0200

    Script added

--- main.cc
+++ main.cc
@@ -5,21 +5,21 @@
 using namespace std;
 
 int main(int argc, char** argv){
-   srand(time(NULL));
-    
-    Graph g;
-    //g.read_graph();
-    g.createNxN(3);
-    //g.print_graph();
-    cout<<"Num connex components = "<<g.numCompConnex()<<endl;
-    double q;
-    cin>>q;
-    cout<<endl;
-    cout<<"Q = "<<q<<endl;
-    cout<<endl;
-    g.precolacio_graf_aresta(q);
-    g.precolacio_graf_node(q);
-    //g.print_graph();
-    cout<<"Num connex components = "<<g.numCompConnex()<<endl;
-    return 0;
+  srand((unsigned)time(NULL)%100*getpid());
+  if (argc < 3){
+    cout<<"Usage: n q"<<endl;
+    return -1;
+  }
+  int n = atoi(argv[1]);
+  double q = stod(argv[2]);
+
+  Graph g;
+  g.createNxN(n);
+  int ant = g.numCompConnex();
+  g.precolacio_graf_aresta(q);
+  g.precolacio_graf_node(q);
+  int act = g.numCompConnex();
+
+  cout<<(ant!=act)<<endl;
+  return 0;
 }
