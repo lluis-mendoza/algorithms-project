8611647300cc2ad36d61235e49875cffeca77872
Pablo <pabloortiz132@gmail.com>
2020-10-21 00:00:38 +0200

    Delete unnecessary, edit model d

--- modeloD.cc
+++ modeloD.cc
@@ -8,24 +8,19 @@ using namespace std;
 int main(){
   srand((unsigned)time(NULL)%100*getpid());
   int n;
+  int r;
   int num;
   char a;
   bool prec_arist = false;
   bool prec_nodos = false;
   cout<<"Numero de nodos:"<<endl;
   cin>>n;
+  cout<<"Parámetro r:"<<endl;
+  cin>>r;
   Graph2D rgg;
-  rgg.createRGG(n,0.2);
-  rgg.print_graph();
-
+  rgg.createRGG(n,r);
   cout<<"Numero de ejecuciones:"<<endl;
   cin>>num;
-  cout<<"Precolacion de aristas? (S/N)"<<endl;
-  cin>>a;
-  prec_arist = (a=='S'||a=='s');
-  cout<<"Precolacion de nodos? (S/N)"<<endl;
-  cin>>a;
-  prec_nodos = (a=='S'||a=='s');
 
   double q = 0;
   while(q <= 1.01){
@@ -33,8 +28,7 @@ int main(){
     for(int i = 0; i<num; ++i){
       Graph g;
       g.createNxN(n);
-      if (prec_arist) g.precolacio_graf_aresta(q);
-      if (prec_nodos) g.precolacio_graf_node(q);
+      g.precolacio_graf_aresta(q);
       numTotalCompConnex += (float)g.numCompConnex()/(n*n);
     }
     cout<<"Probabilidad q = "<<q;
